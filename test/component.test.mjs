ab398f7605689075d59eeb78849fcfbba197ae33
Tim Nimets <tymnim@gmail.com>
2022-11-09 23:36:46 -0500

    component #then tests

--- test/component.test.mjs
+++ test/component.test.mjs
@@ -21,34 +21,93 @@ const todoList = component(todos => {
 });
 
 export default Tests("component",
-  Test("basic component, static content (todo list)", async () => {
-    const task1 = { text: "feed cat", done: new reactive(false) };
-    const task2 = { text: "clean room", done: new reactive(false) };
-    const task3 = { text: "finish assignment", done: new reactive(false) };
+  Tests("Base",
+    Test("basic component, static content (todo list)", async () => {
+      const task1 = { text: "feed cat", done: new reactive(false) };
+      const task2 = { text: "clean room", done: new reactive(false) };
+      const task3 = { text: "finish assignment", done: new reactive(false) };
 
-    const items = () => [task1, task2, task3];
+      const items = () => [task1, task2, task3];
 
-    const list = await todoList(items);
+      const list = await todoList(items);
 
-    assert.equal(list.toString(), `<ul><li class="grey">feed cat</li><li class="grey">clean room</li><li class="grey">finish assignment</li></ul>`);
-    await task1.done.set(true);
-    assert.equal(list.toString(), `<ul><li class="green">feed cat</li><li class="grey">clean room</li><li class="grey">finish assignment</li></ul>`);
-    await task2.done.set(true);
-    assert.equal(list.toString(), `<ul><li class="green">feed cat</li><li class="green">clean room</li><li class="grey">finish assignment</li></ul>`);
+      assert.equal(list.toString(), `<ul><li class="grey">feed cat</li><li class="grey">clean room</li><li class="grey">finish assignment</li></ul>`);
+      await task1.done.set(true);
+      assert.equal(list.toString(), `<ul><li class="green">feed cat</li><li class="grey">clean room</li><li class="grey">finish assignment</li></ul>`);
+      await task2.done.set(true);
+      assert.equal(list.toString(), `<ul><li class="green">feed cat</li><li class="green">clean room</li><li class="grey">finish assignment</li></ul>`);
 
-  }),
-  Test("basic component, dynamic content (todo list)", async () => {
-    const task1 = { text: "feed cat", done: new reactive(false) };
+    }),
+    Test("basic component, dynamic content (todo list)", async () => {
+      const task1 = { text: "feed cat", done: new reactive(false) };
 
-    const [items,,setItems] = reactive([]);
+      const [items,,setItems] = reactive([]);
 
-    const list = await div({}, todoList(items));
+      const list = await div({}, todoList(items));
 
-    assert.equal(list.toString(), `<div><span>No items found</span></div>`);
-    await setItems(items => items.concat(task1));
-    assert.equal(list.toString(), `<div><ul><li class="grey">feed cat</li></ul></div>`);
-    await task1.done.set(true);
-    assert.equal(list.toString(), `<div><ul><li class="green">feed cat</li></ul></div>`);
-  }),
+      assert.equal(list.toString(), `<div><span>No items found</span></div>`);
+      await setItems(items => items.concat(task1));
+      assert.equal(list.toString(), `<div><ul><li class="grey">feed cat</li></ul></div>`);
+      await task1.done.set(true);
+      assert.equal(list.toString(), `<div><ul><li class="green">feed cat</li></ul></div>`);
+    }),
+  ),
+  Tests("#then (rendered)",
+    Test("single then", async () => {
+      const success = Symbol("success");
+
+      const rendered = await new Promise(async resolve => {
+        const [state, setState] = reactive();
+        reactive(scope => {
+          const value = state();
+          if (!scope.firstRun) {
+            scope.stop();
+            resolve(value);
+          }
+        });
+        const task1 = { text: "feed cat", done: new reactive(false) };
+        const [items,,setItems] = reactive([task1]);
+        todoList.then((list) => {
+          assert.equal(list.toString(), `<ul><li class="grey">feed cat</li></ul>`);
+          setState(success);
+        });
+
+        const list = div({}, todoList(items));
+        assert.notEqual(state(), success);
+        assert.equal((await list).toString(), `<div><ul><li class="grey">feed cat</li></ul></div>`);
+      });
+
+      assert.equal(rendered, success);
+    }),
+    Test("chaining then", async () => {
+      const success = Symbol("success");
+
+      const rendered = await new Promise(async resolve => {
+        const [state, setState] = reactive();
+        reactive(scope => {
+          const value = state();
+          if (!scope.firstRun) {
+            scope.stop();
+            resolve(value);
+          }
+        });
+        const task1 = { text: "feed cat", done: new reactive(false) };
+        const [items,,setItems] = reactive([task1]);
+        todoList.then((list, contextData) => {
+          assert.equal(list.toString(), `<ul><li class="grey">feed cat</li></ul>`);
+          return contextData;
+        }).then((data, d) => {
+          assert.equal(data, items, "data's not the same");
+          setState(success);
+        });
+
+        const list = div({}, todoList(items));
+        assert.notEqual(state(), success);
+        assert.equal((await list).toString(), `<div><ul><li class="grey">feed cat</li></ul></div>`);
+      });
+
+      assert.equal(rendered, success);
+    })
+  )
 );
 
