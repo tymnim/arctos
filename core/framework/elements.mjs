2abd1602237afb02fa02dff9413d42f0809b443e
Tim Nimets <tymnim@gmail.com>
2022-11-17 17:43:54 -0500

    all html elements

--- core/framework/elements.mjs
+++ core/framework/elements.mjs
@@ -1,38 +1,327 @@
 
 import { element } from "./element.mjs";
 
-export function div(...args) {
-  return element("div", args);
+export function a(...args) {
+  return element("a", args);
 }
-
-export function span(...args) {
-  return element("span", args);
+export function abbr(...args) {
+  return element("abbr", args);
 }
-
+export function address(...args) {
+  return element("address", args);
+}
+export function area(...args) {
+  return element("area", args);
+}
+export function article(...args) {
+  return element("article", args);
+}
+export function audio(...args) {
+  return element("audio", args);
+}
+export function b(...args) {
+  return element("b", args);
+}
+export function base(...args) {
+  return element("base", args);
+}
+export function bdi(...args) {
+  return element("bdi", args);
+}
+export function bdo(...args) {
+  return element("bdo", args);
+}
+export function blockquote(...args) {
+  return element("blockquote", args);
+}
+export function body(...args) {
+  return element("body", args);
+}
+export function br(...args) {
+  return element("br", args);
+}
 export function button(...args) {
   return element("button", args);
 }
-
-export function input(...args) {
-  return element("input", args);
+export function canvas(...args) {
+  return element("canvas", args);
 }
-
-export function main(...args) {
-  return element("main", args);
+export function caption(...args) {
+  return element("caption", args);
 }
-
+export function cite(...args) {
+  return element("cite", args);
+}
+export function code(...args) {
+  return element("code", args);
+}
+export function col(...args) {
+  return element("col", args);
+}
+export function colgroup(...args) {
+  return element("colgroup", args);
+}
+export function data(...args) {
+  return element("data", args);
+}
+export function datalist(...args) {
+  return element("datalist", args);
+}
+export function dd(...args) {
+  return element("dd", args);
+}
+export function del(...args) {
+  return element("del", args);
+}
+export function details(...args) {
+  return element("details", args);
+}
+export function dfn(...args) {
+  return element("dfn", args);
+}
+export function dialog(...args) {
+  return element("dialog", args);
+}
+export function div(...args) {
+  return element("div", args);
+}
+export function dl(...args) {
+  return element("dl", args);
+}
+export function dt(...args) {
+  return element("dt", args);
+}
+export function em(...args) {
+  return element("em", args);
+}
+export function dmbed(...args) {
+  return element("dmbed", args);
+}
+export function fieldset(...args) {
+  return element("fieldset", args);
+}
+export function figcaption(...args) {
+  return element("figcaption", args);
+}
+export function figure(...args) {
+  return element("figure", args);
+}
+export function footer(...args) {
+  return element("footer", args);
+}
+export function form(...args) {
+  return element("form", args);
+}
+export function h1(...args) {
+  return element("h1", args);
+}
+export function h2(...args) {
+  return element("h2", args);
+}
+export function h3(...args) {
+  return element("h3", args);
+}
+export function h4(...args) {
+  return element("h4", args);
+}
+export function h5(...args) {
+  return element("h5", args);
+}
+export function h6(...args) {
+  return element("h6", args);
+}
+export function head(...args) {
+  return element("head", args);
+}
+export function header(...args) {
+  return element("header", args);
+}
+export function hr(...args) {
+  return element("hr", args);
+}
+export function html(...args) {
+  return element("html", args);
+}
+export function i(...args) {
+  return element("i", args);
+}
+export function iframe(...args) {
+  return element("iframe", args);
+}
 export function img(...args) {
   return element("img", args);
 }
-
-export function ul(...args) {
-  return element("ul", args);
+export function input(...args) {
+  return element("input", args);
 }
-
+export function ins(...args) {
+  return element("ins", args);
+}
+export function kbd(...args) {
+  return element("kbd", args);
+}
+export function label(...args) {
+  return element("label", args);
+}
+export function legend(...args) {
+  return element("legend", args);
+}
+export function li(...args) {
+  return element("li", args);
+}
+export function link(...args) {
+  return element("link", args);
+}
+export function main(...args) {
+  return element("main", args);
+}
+export function map(...args) {
+  return element("map", args);
+}
+export function mark(...args) {
+  return element("mark", args);
+}
+export function meta(...args) {
+  return element("meta", args);
+}
+export function meter(...args) {
+  return element("meter", args);
+}
+export function nav(...args) {
+  return element("nav", args);
+}
+export function noscript(...args) {
+  return element("noscript", args);
+}
+export function object(...args) {
+  return element("object", args);
+}
 export function ol(...args) {
   return element("ol", args);
 }
-
-export function li(...args) {
-  return element("li", args);
+export function optgroup(...args) {
+  return element("optgroup", args);
+}
+export function option(...args) {
+  return element("option", args);
+}
+export function output(...args) {
+  return element("output", args);
+}
+export function p(...args) {
+  return element("p", args);
+}
+export function param(...args) {
+  return element("param", args);
+}
+export function picture(...args) {
+  return element("picture", args);
+}
+export function pre(...args) {
+  return element("pre", args);
+}
+export function progress(...args) {
+  return element("progress", args);
+}
+export function q(...args) {
+  return element("q", args);
+}
+export function rp(...args) {
+  return element("rp", args);
+}
+export function rt(...args) {
+  return element("rt", args);
+}
+export function ruby(...args) {
+  return element("ruby", args);
+}
+export function s(...args) {
+  return element("s", args);
+}
+export function samp(...args) {
+  return element("samp", args);
+}
+export function script(...args) {
+  return element("script", args);
+}
+export function section(...args) {
+  return element("section", args);
+}
+export function select(...args) {
+  return element("select", args);
+}
+export function small(...args) {
+  return element("small", args);
+}
+export function source(...args) {
+  return element("source", args);
+}
+export function span(...args) {
+  return element("span", args);
+}
+export function strong(...args) {
+  return element("strong", args);
+}
+export function style(...args) {
+  return element("style", args);
+}
+export function sub(...args) {
+  return element("sub", args);
+}
+export function summary(...args) {
+  return element("summary", args);
+}
+export function sup(...args) {
+  return element("sup", args);
+}
+export function svg(...args) {
+  return element("svg", args);
+}
+export function table(...args) {
+  return element("table", args);
+}
+export function tbody(...args) {
+  return element("tbody", args);
+}
+export function td(...args) {
+  return element("td", args);
+}
+export function template(...args) {
+  return element("template", args);
+}
+export function textarea(...args) {
+  return element("textarea", args);
+}
+export function tfoot(...args) {
+  return element("tfoot", args);
+}
+export function th(...args) {
+  return element("th", args);
+}
+export function thead(...args) {
+  return element("thead", args);
+}
+export function time(...args) {
+  return element("time", args);
+}
+export function title(...args) {
+  return element("title", args);
+}
+export function tr(...args) {
+  return element("tr", args);
+}
+export function track(...args) {
+  return element("track", args);
+}
+export function u(...args) {
+  return element("u", args);
+}
+export function ul(...args) {
+  return element("ul", args);
+}
+export function video(...args) {
+  return element("video", args);
+}
+export function wbr(...args) {
+  return element("wbr", args);
 }
