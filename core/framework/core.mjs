bd1252d3caf0cb3b4e0dc8e5a7e882e89c6f22b9
Tim Nimets <tymnim@gmail.com>
2022-11-17 23:53:20 -0500

    npm alpha

--- core/framework/core.mjs
+++
@@ -1,5 +0,0 @@
-
-export { component } from "./component.mjs";
-export { Body } from "./window.mjs";
-export { span, button, input, div, main, img } from "./elements.mjs";
-export { render } from "./renderer.mjs";
---
+++ index.mjs
@@ -0,0 +1,5 @@
+export { component } from "./core/framework/component.mjs";
+export { element } from "./core/framework/element.mjs";
+export { render, mount } from "./core/framework/renderer.mjs";
+export { reactive, nonreactive } from "./core/reactivity/hooks.mjs";
+export * from "./core/framework/elements.mjs";
