e5566c23647cdc67ec9c616685d215a7c12c3d2b
Tim Nimets <tymnim@gmail.com>
2022-11-18 20:16:01 -0500

    fixed renderer to render uncalled components

--- core/framework/renderer.mjs
+++ core/framework/renderer.mjs
@@ -4,12 +4,13 @@ export const render = Symbol("renderer");
 const isSSR = typeof global !== "undefined" && !global.document && !global.window;
 
 async function renderer(...components) {
-  for (let i in await Promise.all(components)) {
+  const promises = components.filter(c => c instanceof Promise);
+  const thenables = components.filter(c => !(c instanceof Promise));
+  for (let i in (await Promise.all(promises)).concat(thenables)) {
     const component = components[i] instanceof Function ? components[i]() : components[i];
     this.appendChild(await component);
   }
 }
-
 // TODO: SSR option
 if (! isSSR) {
   Object.defineProperty(Node.prototype, render, { get() { return renderer.bind(this) } });
