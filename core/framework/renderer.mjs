f9bd1b9e633ca8703272a3943d3fd67c1579ac3a
Tim Nimets <tymnim@gmail.com>
2022-11-27 12:57:21 -0500

    fixed renderer; added css to components

--- core/framework/renderer.mjs
+++ core/framework/renderer.mjs
@@ -1,14 +1,15 @@
 
+import { normalize } from "./utils.mjs";
+
 export const render = Symbol("renderer");
 
 const isSSR = typeof global !== "undefined" && !global.document && !global.window;
 
 async function renderer(...components) {
-  const promises = components.filter(c => c instanceof Promise);
-  const thenables = components.filter(c => !(c instanceof Promise));
-  for (let i in (await Promise.all(promises)).concat(thenables)) {
-    const component = components[i] instanceof Function ? components[i]() : components[i];
-    this.appendChild(await component);
+  for (let component of components) {
+    const c = component instanceof Promise ? await component : component;
+    const elements = await (component instanceof Function ? component() : component);
+    (await Promise.all(normalize(elements))).forEach(element => this.appendChild(element))
   }
 }
 // TODO: SSR option
@@ -16,4 +17,5 @@ if (! isSSR) {
   Object.defineProperty(Node.prototype, render, { get() { return renderer.bind(this) } });
 }
 
+// TODO: body creating and parsing from html
 export const mount = !isSSR ? document.body[render] : undefined;
