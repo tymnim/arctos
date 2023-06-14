6008f8d0f441af36213d17ea6d3dbc4222359aba
Tim Nimets <tymnim@gmail.com>
2023-06-14 18:25:02 -0400

    rid of folder nesting

--- index.mjs
+++ index.mjs
@@ -1,7 +1,8 @@
-export { component } from "./core/framework/component.mjs";
-export { element } from "./core/framework/element.mjs";
-export { render, mount } from "./core/framework/renderer.mjs";
-export * from "./core/framework/elements.mjs";
-export { store, useStore } from "./core/framework/stores.mjs";
-export { importCss as css, importFromFile as importCssFrom, parse, cssRegistery} from "./core/framework/cssParser.mjs";
-export { focus } from "./core/framework/utils.mjs";
+
+export { component } from "./core/component.mjs";
+export { element } from "./core/element.mjs";
+export { render, mount } from "./core/renderer.mjs";
+export * from "./core/elements.mjs";
+export { store, useStore } from "./core/stores.mjs";
+export { importCss as css, importFromFile as importCssFrom, parse, cssRegistery} from "./core/cssParser.mjs";
+export { focus } from "./core/utils.mjs";
